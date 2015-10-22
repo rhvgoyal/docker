ff8fecfd9b42703619c4335297317f2031c01bab
Dan Walsh <dwalsh@redhat.com>
2015-10-22 12:14:23 -0400

    Allow for remote repository inspection
    **Inspect remote images**
    
    Extended server API to allow for inspection of remote images. If a short
    name is given, list of additional registries will be queried until a
    repository is found. This allows for remote inspection without
    downloading image layers.
    
    The URL is following:
    
        /<apiVersion>/images/<repoName>/json?remote=1
    
    The output is the same json as for local images. However, it excludes
    few attributes specific only to local images:
    
    - GraphDriver
    - VirtualSize
    
    And it adds some additional attributes:
    
    - Digest
    - Tag
    - Registry
    
    **List tags of remote repository**
    
    Extended server API to allow for listing of remote repositories.
    
    The URL is following:
    
        /<apiVersion>/images/<repoName>/tags?remote=1
    
    If a short name is given, list of additional registries will be queried
    until a repository is found.
    
    Returned is a json in following format:
    
        { "Name"    : "<repoName>"
        , "TagList" : [ {"<tag1>" : "<ID1>", "<tag2>" : <ID2>" } ]
        }
    
    Quirks and TODOs:
     - By default V1 registry enpoints are queried because they provide tags
       with associated image IDs. If not available, V2 endpoint will be
       tried. In such a case the output will lack image IDs though.
    
    Signed-off-by: Michal Minar <miminar@redhat.com>
    
    Signed-off-by: Dan Walsh <dwalsh@redhat.com>

--- integration-cli/docker_api_inspect_test.go
+++ integration-cli/docker_api_inspect_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"reflect"
 	"strings"
 
 	"github.com/docker/docker/api/types"
@@ -132,4 +134,74 @@ func (s *DockerSuite) TestInspectApiImageResponse(c *check.C) {
 
 	c.Assert(stringutils.InSlice(imageJSON.Tags, "busybox:latest"), check.Equals, true)
 	c.Assert(stringutils.InSlice(imageJSON.Tags, "busybox:mytag"), check.Equals, true)
+}
+
+func (s *DockerRegistrySuite) TestInspectApiRemoteImage(c *check.C) {
+	repoName := fmt.Sprintf("%v/dockercli/busybox", s.reg.url)
+	// tag the image and upload it to the private registry
+	dockerCmd(c, "tag", "busybox", repoName)
+	defer deleteImages(repoName)
+
+	dockerCmd(c, "push", repoName)
+	localValue, _, _ := apiCallInspectImage(c, nil, repoName, false, false)
+	remoteValue, _, _ := apiCallInspectImage(c, nil, repoName, true, false)
+	compareInspectValues(c, "a", localValue, remoteValue, true)
+
+	deleteImages(repoName)
+
+	// local inspect shall fail now
+	_, status, _ := apiCallInspectImage(c, nil, repoName, false, true)
+	c.Assert(status, check.Equals, http.StatusNotFound)
+
+	// remote inspect shall still succeed
+	remoteValue2, _, _ := apiCallInspectImage(c, nil, repoName, true, false)
+	compareInspectValues(c, "a", localValue, remoteValue2, true)
+}
+
+func (s *DockerRegistrySuite) TestInspectApiImageFromAdditionalRegistry(c *check.C) {
+	d := NewDaemon(c)
+	daemonArgs := []string{"--add-registry=" + s.reg.url}
+	if err := d.StartWithBusybox(daemonArgs...); err != nil {
+		c.Fatalf("we should have been able to start the daemon with passing { %s } flags: %v", strings.Join(daemonArgs, ", "), err)
+	}
+	defer d.Stop()
+
+	repoName := fmt.Sprintf("dockercli/busybox")
+	fqn := s.reg.url + "/" + repoName
+	// tag the image and upload it to the private registry
+	if out, err := d.Cmd("tag", "busybox", fqn); err != nil {
+		c.Fatalf("image tagging failed: %s, %v", out, err)
+	}
+
+	localValue, _, _ := apiCallInspectImage(c, d, repoName, false, false)
+
+	_, status, _ := apiCallInspectImage(c, d, repoName, true, true)
+	c.Assert(status, check.Equals, http.StatusNotFound)
+
+	if out, err := d.Cmd("push", fqn); err != nil {
+		c.Fatalf("failed to push image %s: error %v, output %q", fqn, err, out)
+	}
+
+	remoteValue, _, _ := apiCallInspectImage(c, d, repoName, true, false)
+	compareInspectValues(c, "a", localValue, remoteValue, true)
+
+	if out, err := d.Cmd("rmi", fqn); err != nil {
+		c.Fatalf("failed to remove image %s: %s, %v", fqn, out, err)
+	}
+
+	remoteValue2, _, _ := apiCallInspectImage(c, d, fqn, true, false)
+	compareInspectValues(c, "a", localValue, remoteValue2, true)
+}
+
+func (s *DockerRegistrySuite) TestInspectApiNonExistentRepository(c *check.C) {
+	repoName := fmt.Sprintf("%s/foo/non-existent", s.reg.url)
+
+	_, status, err := apiCallInspectImage(c, nil, repoName, false, true)
+	c.Assert(status, check.Equals, http.StatusNotFound)
+	c.Assert(err, check.Not(check.IsNil))
+	c.Assert(err.Error(), check.Matches, `(?i)no such image.*`)
+
+	_, status, err = apiCallInspectImage(c, nil, repoName, true, true)
+	c.Assert(err, check.Not(check.IsNil))
+	c.Assert(err.Error(), check.Matches, `(?i).*(not found|no such image|no tags available).*`)
 }
